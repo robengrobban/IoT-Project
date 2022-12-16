5cc661844b13690aa0aab29dac27cd23e7cdb476
Robert Englund <kastanjen3@gmail.com>
2022-12-16 09:52:04 +0100

    Update sensor.py
    Co-Authored-By: jes-blo <117733461+jes-blo@users.noreply.github.com>
    Co-Authored-By: Lekhaz Adapa <lekhazadapa18@gmail.com>

--- SesnorScript/sensor.py
+++ SesnorScript/sensor.py
@@ -4,11 +4,14 @@ import json
 import requests
 import paho.mqtt.client as mqtt
 import paho.mqtt.publish as publish
+import board
+import adafruit_vcnl4010
+import adafruit_tca9548a
 
 #broker = "83.226.147.68"
 broker = "tcp://83.226.147.68:1883"
 
-#Publish format
+#Publish format (JSON string)
 #{
 #   id: '{id}',
 #   availableSeats: '{number}',
